9cb723cb246b9d8b6d219046fe61e798bd1869ee
liuketao <89596593@qq.com>
2024-10-24 19:02:08 +0800

    Update AuthButton.jsx

--- components/AuthButton.jsx
+++ components/AuthButton.jsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import { PlasmicAuthButton } from "./plasmic/free_fire_simulator/PlasmicAuthButton";
 
 function AuthButton_(props, ref) {
+  const { onClick, ...rest } = props; // 提取 onClick 和其他 props
   const { plasmicProps } = PlasmicAuthButton.useBehavior(props, ref);
-  return <PlasmicAuthButton {...plasmicProps} />;
+  return <PlasmicAuthButton {...plasmicProps} onClick={onClick} />;
 }
 
 const AuthButton = React.forwardRef(AuthButton_);
