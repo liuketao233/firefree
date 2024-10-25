f9cda06271deb36cfed0487932044735cc1b80bb
liuketao <89596593@qq.com>
2024-10-25 09:43:32 +0800

    Update PlasmicGameBalance.jsx

--- components/plasmic/free_fire_simulator/PlasmicGameBalance.jsx
+++ components/plasmic/free_fire_simulator/PlasmicGameBalance.jsx
@@ -50,6 +50,7 @@ function useNextRouter() {
 
 function PlasmicGameBalance__RenderFunc(props) {
   const { variants, overrides, forNode } = props;
+  console.log(overrides,"PlasmicGameBalance__RenderFunc")
   const $translator = usePlasmicTranslator?.();
   const args = React.useMemo(
     () =>
