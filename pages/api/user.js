cb8155c6f194862fc6345ca04b62fa2661387a6a
liuketao <89596593@qq.com>
2024-10-28 08:32:28 +0800

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -7,21 +7,21 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("sample_mflix");
   const session = await getSession({ req });
+  if(!session){
+    res.json({ status: 500, message: 'User unfind' }).end();
+  }
     
   switch (req.method) {
     case 'GET':
-    //   const { name } = req.query;
-    //   let findData = {};
-    //   if(name && name != ''){
-    //     findData = {name : name};
-    //   }
-    //   const users = await db.collection('users').find(findData).toArray();
-      res.json({ status: 200, data: session });
+
+      const user = session.user;
+      const users = await db.collection('users').find({email:user.email}).toArray();
+      res.json({ status: 200, data: users });
       break;
     case 'POST':
       const newUser = req.body;
       await db.collection('users').insertOne(newUser);
-      res.json({ status: 201, message: 'User added!' });
+      res.json({ status: 200, message: 'User added!' });
       break;
     default:
       res.status(405).end(); // Method Not Allowed
