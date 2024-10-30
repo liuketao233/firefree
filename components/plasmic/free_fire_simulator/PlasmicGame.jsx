40974577a38fb6335de0c31bfc96ba0c362bd8f6
liuketao <89596593@qq.com>
2024-10-30 14:10:27 +0800

    Update PlasmicGame.jsx

--- components/plasmic/free_fire_simulator/PlasmicGame.jsx
+++ components/plasmic/free_fire_simulator/PlasmicGame.jsx
@@ -238,11 +238,7 @@ function PlasmicGame__RenderFunc(props) {
             data-plasmic-name={"gameBody"}
             overrides={{
               balance:apiData && apiData.data ?apiData.data[0].balance : 0,
-              infoArea:{
-                props:{
-                  recentRecords:gameArray
-                }
-              }
+              recentRecords:gameArray
             }}
             data-plasmic-override={overrides.gameBody}
             className={classNames("__wab_instance", sty.gameBody)}
